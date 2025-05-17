577a4f07b40cb0f43423221025848091850e55cb
Faseen Suhain <faseensuhain123@gmail.com>
2025-05-17 19:12:05 +0530

    new module added

---
+++ yariz_hospital/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Om hospital',
+    'version' : '1.2',
+    'summary': '',
+    'sequence': 10,
+    'description': """
+    """,
+    'category': '',
+    'website': '',
+    'depends': [],
+    'data': [
+
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'assets': {
+
+    },
+    'license': 'LGPL-3',
+}
